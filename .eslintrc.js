e1e86bbfe23840ed72111d054b6f8815ba3e9141
Martin Strauss <martin@ockle.org>
2020-08-30 18:16:56 -0700

    Add an .eslintrc.js

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2020': true,
+  },
+  "extends": [
+    "closure"
+  ],
+  'parserOptions': {
+    'ecmaVersion': 12,
+  },
+  'rules': {
+  },
+};

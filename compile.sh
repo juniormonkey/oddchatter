d896ba5b4b56b4e008ebd4c1f9426d32ca92c659
Martin Strauss <martin@ockle.org>
2020-09-23 12:32:42 -0700

    move public/scripts/src to top level, and update compile scripts

--- compile.sh
+++ compile.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-cd public/scripts
+rm -rf public/scripts/src
+cp src public/scripts/src
 npx google-closure-compiler \
   --platform=java \
   --externs=src/externs.js \
-  --externs=../../node_modules/firebase/externs/firebase-app-externs.js \
-  --externs=../../node_modules/firebase/externs/firebase-auth-externs.js \
-  --externs=../../node_modules/firebase/externs/firebase-client-auth-externs.js \
-  --externs=../../node_modules/firebase/externs/firebase-error-externs.js \
-  --externs=../../node_modules/firebase/externs/firebase-externs.js \
-  --externs=../../node_modules/firebase/externs/firebase-firestore-externs.js \
-  --create_source_map=main.js.map \
-  --js_output_file=main.js \
-  --js='../../node_modules/google-closure-library/closure/goog/**.js' \
-  --js='../../node_modules/google-closure-library/closure/third_party/**.js' \
+  --externs=node_modules/firebase/externs/firebase-app-externs.js \
+  --externs=node_modules/firebase/externs/firebase-auth-externs.js \
+  --externs=node_modules/firebase/externs/firebase-client-auth-externs.js \
+  --externs=node_modules/firebase/externs/firebase-error-externs.js \
+  --externs=node_modules/firebase/externs/firebase-externs.js \
+  --externs=node_modules/firebase/externs/firebase-firestore-externs.js \
+  --create_source_map=public/scripts/main.js.map \
+  --js_output_file=public/scripts/main.js \
+  --js='node_modules/google-closure-library/closure/goog/**.js' \
+  --js='node_modules/google-closure-library/closure/third_party/**.js' \
   --js='src/**.js' \
   --js='!src/*externs.js' \
   --dependency_mode=PRUNE \

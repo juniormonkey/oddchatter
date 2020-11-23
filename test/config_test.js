86745fceadc9861f3be92604cb8adf4cf9507f36
Martin Strauss <martin@ockle.org>
2020-11-22 17:08:59 -0800

    Make percentage threshold minimum 1, and add a test to verify

---
+++ test/config_test.js
@@ -0,0 +1,39 @@
+/* eslint-disable closure/no-undef */
+
+import {
+  CONFIG,
+} from '../src/config.js';
+
+const chai = require('chai');
+chai.should();
+
+describe('config', function() {
+
+  it('returns a callback threshold at least 1', function() {
+    CONFIG.active_users = 25;
+    CONFIG.callback_threshold_raw = 0;
+    CONFIG.threshold_is_percentage = false;
+    CONFIG.callbackThreshold().should.equal(1);
+  });
+
+  it('returns threshold when not a percentage', function() {
+    CONFIG.active_users = 25;
+    CONFIG.callback_threshold_raw = 3;
+    CONFIG.threshold_is_percentage = false;
+    CONFIG.callbackThreshold().should.equal(3);
+  });
+  
+  it('returns threshold when percentage requested', function() {
+    CONFIG.active_users = 25;
+    CONFIG.callback_threshold_raw = 15;
+    CONFIG.threshold_is_percentage = true;
+    CONFIG.callbackThreshold().should.equal(3);
+  });
+  
+  it('returns threshold at least 1 when percentage requested', function() {
+    CONFIG.active_users = 25;
+    CONFIG.callback_threshold_raw = 3;
+    CONFIG.threshold_is_percentage = true;
+    CONFIG.callbackThreshold().should.equal(1);
+  });
+});
